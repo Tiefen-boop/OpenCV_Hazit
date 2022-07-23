b28bce9dc4124a7303282e88f2c25f51a34a16d9
Ron Ziskind <2002ron10@gmail.com>
2022-07-23 20:07:48 +0300

    finished the unique lines functionality

---
+++ helpFunctions.py
@@ -0,0 +1,108 @@
+from collections import defaultdict
+
+import cv2
+import numpy as np
+
+def linesToMap(lines):
+    map={}
+    for line in lines:
+        radian=round(line[0][1],4)
+        radius=line[0][0]
+        if (radian in map.keys()):
+            map.get(radian).append(radius)
+            #map[radian]=map.get(radian).append(radius)
+        else:
+            map[radian]=[radius]
+    return linesMaptoFinalLines(map)
+def average(l):
+    return sum(l) / len(l)
+def linesMaptoFinalLines(map):
+    finalLines=[]
+    for radian in map:
+        valuesForRad=[]#this will be a 2d array
+        for radius in map[radian]:#iterates over all the values of this radius(an array of floats)
+            foundValueArrayForRadius=False
+            for lineOfKey in valuesForRad:#previous values found. line of key is also an array
+                if(abs(radius- average( lineOfKey))<6):
+                    lineOfKey.append(radius)
+                    foundValueArrayForRadius=True
+                    break
+            if(not foundValueArrayForRadius):
+                valuesForRad.append([radius])
+
+        for val in valuesForRad:
+            finalLines.append([[average(val),radian]])
+    return finalLines
+
+# def uniqueLines(lines):
+#     seenLines=[]
+#     addLine=True
+#     for line in lines:
+#         for seenline in seenLines:
+#             if(round(seenline[0][1],3)==round(line[0][1],3)):
+#                 addLine=False
+#                 break
+#         if(addLine):
+#             seenLines.append(line)
+#         addLine=True;
+#     return seenLines
+
+def segment_by_angle_kmeans(lines, k=2, **kwargs):
+    """Groups lines based on angle with k-means.
+
+    Uses k-means on the coordinates of the angle on the unit circle
+    to segment `k` angles inside `lines`.
+    """
+
+    # Define criteria = (type, max_iter, epsilon)
+    default_criteria_type = cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER
+    criteria = kwargs.get('criteria', (default_criteria_type, 10, 1.0))
+    flags = kwargs.get('flags', cv2.KMEANS_RANDOM_CENTERS)
+    attempts = kwargs.get('attempts', 10)
+
+    # returns angles in [0, pi] in radians
+    angles = np.array([line[0][1] for line in lines])
+    # multiply the angles by two and find coordinates of that angle
+    pts = np.array([[np.cos(2*angle), np.sin(2*angle)]
+                    for angle in angles], dtype=np.float32)
+
+    # run kmeans on the coords
+    labels, centers = cv2.kmeans(pts, k, None, criteria, attempts, flags)[1:]
+    labels = labels.reshape(-1)  # transpose to row vec
+
+    # segment lines based on their kmeans label
+    segmented = defaultdict(list)
+    for i, line in enumerate(lines):
+        segmented[labels[i]].append(line)
+    segmented = list(segmented.values())
+    return segmented
+
+def intersection(line1, line2):
+    """Finds the intersection of two lines given in Hesse normal form.
+
+    Returns closest integer pixel locations.
+    See https://stackoverflow.com/a/383527/5087436
+    """
+    rho1, theta1 = line1[0]
+    rho2, theta2 = line2[0]
+    A = np.array([
+        [np.cos(theta1), np.sin(theta1)],
+        [np.cos(theta2), np.sin(theta2)]
+    ])
+    b = np.array([[rho1], [rho2]])
+    x0, y0 = np.linalg.solve(A, b)
+    x0, y0 = int(np.round(x0)), int(np.round(y0))
+    return [[x0, y0]]
+
+
+def segmented_intersections(lines):
+    """Finds the intersections between groups of lines."""
+
+    intersections = []
+    for i, group in enumerate(lines[:-1]):
+        for next_group in lines[i+1:]:
+            for line1 in group:
+                for line2 in next_group:
+                    intersections.append(intersection(line1, line2))
+
+    return intersections
---
+++ tryingHoughLines.py
@@ -0,0 +1,102 @@
+# Python program to illustrate HoughLine
+# method for line detection
+import cv2
+import numpy as np
+import helpFunctions
+# Reading the required image in
+# which operations are to be done.
+# Make sure that the image is in the same
+# directory in which this python program is
+
+from collections import defaultdict
+
+
+from matplotlib import pyplot as plt
+
+img = cv2.imread('images/download.png')
+
+# Convert the img to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+titles = ['Original Image','grey']
+images = [img,gray]
+for i in range(2):
+    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
+
+# Apply edge detection method on the image
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+
+titles = ['Original Image','grey']
+images = [img,edges]
+for i in range(2):
+    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+# This returns an array of r and theta values
+
+lines = cv2.HoughLines(edges, 1, np.pi / 180, 80 )
+
+# The below for loop runs till r and theta values
+# are in the range of the 2d array
+print(lines)
+lines= helpFunctions.linesToMap(lines)
+print("print final lines \n ")
+print(lines)
+
+for r_theta in lines:
+    arr = np.array(r_theta[0], dtype=np.float64)
+    r, theta = arr
+    # Stores the value of cos(theta) in a
+    a = np.cos(theta)
+
+    # Stores the value of sin(theta) in b
+    b = np.sin(theta)
+
+    # x0 stores the value rcos(theta)
+    x0 = a * r
+
+    # y0 stores the value rsin(theta)
+    y0 = b * r
+
+    # x1 stores the rounded off value of (rcos(theta)-1000sin(theta))
+    x1 = int(x0 + 1000 * (-b))
+
+    # y1 stores the rounded off value of (rsin(theta)+1000cos(theta))
+    y1 = int(y0 + 1000 * (a))
+
+    # x2 stores the rounded off value of (rcos(theta)+1000sin(theta))
+    x2 = int(x0 - 1000 * (-b))
+
+    # y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
+    y2 = int(y0 - 1000 * (a))
+
+
+
+    # cv2.line draws a line in img from the point(x1,y1) to (x2,y2).
+    # (0,0,255) denotes the colour of the line to be
+    # drawn. In this case, it is red.
+    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+
+# All the changes made in the input image are finally
+# written on a new image houghlines.jpg
+segmented = helpFunctions.segment_by_angle_kmeans(lines)
+intersections = helpFunctions.segmented_intersections(segmented)
+
+
+for inter in intersections:
+    cv2.circle(img,(inter[0][0],inter[0][1]),radius=3, color=(0, 255, 0), thickness=-1)
+
+
+
+
+
+
+
+cv2.imwrite('linesDetected.jpg', img)
+
