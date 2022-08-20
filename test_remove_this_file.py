c7e7587fe04b84d7b455613d277e99f474fa76a9
Ron Ziskind <ronzi@post.bgu.ac.il>
2022-08-20 15:16:21 +0300

    fixed is_line_unique_by_avg_distance_using_integral

--- test_remove_this_file.py
+++ test_remove_this_file.py
@@ -1,7 +1,6 @@
-import numpy as np
+def f(a=[]):
+    a.append(5)
+    print(a)
 
-with open(
-        "Computation_For_01562.png_Given_mask_01562_mask.png/Normal_Gradient/O(n^2)/percentile_threshold/hough_space.txt") as textFile:
-    hough_space = [line.split() for line in textFile]
-hough_space = np.array(hough_space, dtype=int)
-print('a')
+f()
+f([])
